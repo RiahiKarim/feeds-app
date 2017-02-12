b8689a3ec6dd012504dbc0faa0f87d23aaabedcc
RIAHI Karim <karim.riahi@ma.ibm.com>
2017-02-12 13:23:16 +0000

    Implemented next/show more button logic

--- scripts/controllers/feedsController.js
+++ scripts/controllers/feedsController.js
@@ -9,13 +9,17 @@
     function feedsCtrl(feedsService,feedsAppConfig) {
         var vm = this;
         vm.config = feedsAppConfig;
+        vm.numberOfFeedsToDisplay = 0;
+        vm.defaultViews = [10,25,50];
+        vm.showPopular = false;
+        vm.search = "";
         vm.feeds = [];
-        vm.loadFeeds = loadFeeds;
         activate();
 
+         //filter videos  that have more than a number of likes
+         //This number is defined in the app config
         vm.popularFeeds = function(){
             return function (feed) {
-                //filter videos from users that have more than 10 likes
                 if ( vm.showPopular === true){
                     return feed.video.numberLikes > feedsAppConfig.popularVideos;
                 }      
@@ -24,16 +28,27 @@
             }
         }
 
-        ////////////////
-        function loadFeeds(numberOfFeedsToLoad){
-            vm.displayedFeeds = numberOfFeedsToLoad;
-            feedsService.getFeeds(vm.displayedFeeds).then(function(feeds){
+        //Load and display  feeds from the data/feeds.json
+        function loadAllFeeds(){
+            feedsService.getFeeds().then(function(feeds){
                 vm.feeds = feeds;
             });
+        }
+
+        //on Click 'Show More' Button, load more feeds 
+        vm.showMoreFeeds = function(){
+            vm.numberOfFeedsToDisplay = vm.defaultViews[vm.defaultViews.indexOf(vm.numberOfFeedsToDisplay) + 1];
+        }
+
+        //Load and display a number of feeds feeds from the data/feeds.json
+        vm.displayFeeds = function(numberOfFeedsToDisplay){
+            vm.numberOfFeedsToDisplay = numberOfFeedsToDisplay;
         };
 
+        //activate this controller and load a default number of feeds
         function activate() { 
-            vm.loadFeeds(feedsAppConfig.defaultDisplayedFeeds);
+            vm.numberOfFeedsToDisplay = feedsAppConfig.defaultDisplayedFeeds;
+            loadAllFeeds(feedsAppConfig.defaultDisplayedFeeds);
         };
 
     }
--- scripts/services/Objects/Video.js
+++ scripts/services/Objects/Video.js
@@ -11,7 +11,7 @@
             this.name = data.name;
             this.link  = data.link;
             this.embedVideo = data.embed.html;
-            this.description = data.description;
+            this.description = data.description || "";
             this.numberPlays = data.stats.plays;
             this.numberComments = data.metadata.connections.comments.total;
             this.numberLikes = data.metadata.connections.likes.total;
